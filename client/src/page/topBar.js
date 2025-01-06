b33a90c7aea9ae4eb9ac263351206a8e912890ca
KimTaeHyun03 <rlaxogus03@gmail.com>
2025-01-06 15:14:34 +0900

    2.0.1

--- client/src/page/topBar.js
+++ client/src/page/topBar.js
@@ -9,9 +9,11 @@ let TopBar = () => {
   let navigate = useNavigate();
   // 슬라이드 메뉴 상태
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-  let [help,setHelp] = useState(false);
-  
+
+  let [help, setHelp] = useState(false);
+  let [android, setAndroid] = useState(false);
+  let [ios, setIos] = useState(false);
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -52,33 +54,81 @@ let TopBar = () => {
         >
           조짜기
         </p>
-        
+
         <p>
           <p className='help'>
-            <h4 onClick={()=>{
-              setHelp(!help);
-            }}>
-              설치 가이드 : {!help?<span>클릭하십쇼</span> : <span>닫을거면 또 클릭하십쇼</span>}
+            <h4
+              onClick={() => {
+                setHelp(!help);
+              }}
+            >
+              설치 가이드
             </h4>
-            {help ? 
-            <ol>
-              <li>크롬으로 열기 버튼 클릭</li>
-              
-              <p className='link'>
-          <Link to='/chrome'>크롬으로 열기</Link>
-        </p>
-        <hr/>
-              <li>크롬이 없다면 크롬 설치해야되는데 알아서 다운로드 페이지로감</li>
-              <hr />
-              <li>우측 상단에 ...세개 새로로 배열되어있는 버튼 클릭</li>
-              <hr />
-              <li>홈 화면에 추가 클릭</li>
-              <hr />
-              <li>이제 어플이 설치 되었을 것이야</li>
-              <hr />
-              <li>설치하면 앞으로 이걸로 편하게 쓸수 있단다 친구들</li>
-              <hr />
-            </ol> : null}
+            <div className='androidIos'>
+              <button
+                onClick={() => {
+                  setAndroid(!android);
+                  setIos(false);
+                }}
+              >
+                android
+              </button>
+              <button
+                onClick={() => {
+                  setIos(!ios);
+                  setAndroid(false);
+                }}
+              >
+                ios
+              </button>
+            </div>
+
+            {android ? (
+              <ol>
+                <li>크롬으로 열기 버튼 클릭</li>
+
+                <p className='link'>
+                  <Link to='/chrome'>크롬으로 열기</Link>
+                </p>
+                <hr />
+                <li>
+                  크롬이 없다면 크롬 설치해야되는데 알아서 다운로드 페이지로감
+                </li>
+                <hr />
+                <li>우측 상단에 ...세개 새로로 배열되어있는 버튼 클릭</li>
+                <hr />
+                <li>홈 화면에 추가 클릭</li>
+                <hr />
+                <li>이제 어플이 설치 되었을 것이야</li>
+                <hr />
+                <li>설치하면 앞으로 이걸로 편하게 쓸수 있단다 친구들</li>
+                <hr />
+              </ol>
+            ) : null}
+
+            {ios ? (
+              <ol>
+                <li>크롬이 없다면 크롬 설치 버튼 클릭</li>
+
+                <p className='link'>
+                  <Link to='/chrome'>크롬 설치</Link>
+                </p>
+                <hr />
+                <li>
+                  크롬 설치 후 링크를 크롬 검색창에 복붙해서 크롬으로 접속
+                  <p className='urlLink'>https://port-0-eagleskp-m5dahxe3d1a3c3c2.sel4.cloudtype.app/</p>
+                </li>
+                <hr />
+                <li>공유버튼 클릭</li>
+                <hr />
+                <li>홈 화면에 추가 클릭</li>
+                <hr />
+                <li>이제 어플이 설치 되었을 것이야</li>
+                <hr />
+                <li>설치하면 앞으로 이걸로 편하게 쓸수 있단다 친구들</li>
+                <hr />
+              </ol>
+            ) : null}
           </p>
         </p>
         <p className='notice'>
@@ -87,23 +137,20 @@ let TopBar = () => {
             1. 관리관님이 지적사항 있는 구역 체크시 푸시알림뜨는 기능 할수있으면
             업데이트 예정...이었는데 지식이 부족하여 불가능할거같습니다
           </p>
+          <p>2. 조리라인을 어떻게 굴릴건지 보여주는건 추후 업데이트 예정</p>
           <p>
-            2. 조리라인을 어떻게 굴릴건지 보여주는건 추후 업데이트 예정
+            3. 아직 미완성이라 메인페이지에서 목록이 안뜨면 새로고침 여러번 하면
+            됨
           </p>
           <p>
-            3. 아직 미완성이라 메인페이지에서 목록이 안뜨면 새로고침 여러번 하면 됨
-          </p>
-          <p>
-            4. 사이트 아무한테나 공유하면 부대 취사병의 이름이 유출될수있으니 절대 누구에게도 공유하지 마십쇼
-            공유하면 사이트 그냥 없애버립니다
+            4. 사이트 아무한테나 공유하면 부대 취사병의 이름이 유출될수있으니
+            절대 누구에게도 공유하지 마십쇼 공유하면 사이트 그냥 없애버립니다
             <p>
-              로그인해서 쓰게끔 할까 했는데 휴대폰으로 개발하는데 어려움이 있어 포기합니다
+              로그인해서 쓰게끔 할까 했는데 휴대폰으로 개발하는데 어려움이 있어
+              포기합니다
             </p>
-            
           </p>
-          <p>
-            5. 오류 발견하면 갠톡주십쇼
-          </p>
+          <p>5. 오류 발견하면 갠톡주십쇼</p>
         </p>
       </div>
 
